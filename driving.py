cb6039d313725ae516ff9334a7bb75ce21975621
JohnsonChi <johnsonchi3689@gmail.com>
2018-03-11 11:49:26 +0800

    add US

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
-country = input('國籍')
-age = input('年齡')
+country = input('國籍: ')
+age = input('年齡: ')
 age = int(age)
 if country == '台灣':
 	if age >= 18:
+		print('許可')
+	else:
+		print('不許可')
+elif country =='美國':
+	if age >= 16:
 		print('許可')
 	else:
 		print('不許可')
